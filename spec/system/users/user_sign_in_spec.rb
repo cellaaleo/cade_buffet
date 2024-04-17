f1dc84aa6d7afab9b2ebafd0e1228d9a8d0f64ea
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-04-17 18:41:41 +0100

    usuário faz log in e log out

---
+++ spec/system/users/user_sign_in_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe "Usuário se autentica" do
+  it "com sucesso" do
+    # Arrange - fazer cadastro
+    User.create!(email: "sac@eventos.com", password: "senha123")
+
+    # Act - fazer log in
+    visit root_path
+    click_on "Entrar"
+    fill_in "E-mail",	with: "sac@eventos.com"
+    fill_in "Senha",	with: "senha123"
+    within('form') do
+      click_on "Entrar"
+    end
+
+    # Assert
+    within('nav') do
+      expect(page).not_to have_link 'Criar sua conta'
+      expect(page).not_to have_link 'Entrar'
+      expect(page).to have_button 'Sair'
+    end
+    expect(page).to have_content 'Login efetuado com sucesso'
+  end
+
+  it "e faz log out" do
+    # Arrange - fazer cadastro
+    User.create!(email: "sac@eventos.com", password: "senha123")
+
+    # Act - fazer log in
+    visit root_path
+    click_on "Entrar"
+    fill_in "E-mail",	with: "sac@eventos.com"
+    fill_in "Senha",	with: "senha123"
+    within('form') do
+      click_on "Entrar"
+    end
+    click_on 'Sair'
+
+    # Assert
+    within('nav') do
+      expect(page).to have_link 'Criar sua conta'
+      expect(page).to have_link 'Entrar'
+      expect(page).not_to have_button 'Sair'
+    end
+    expect(page).to have_content 'Logout efetuado com sucesso'
+  end
+end
