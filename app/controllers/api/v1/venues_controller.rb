68c2aceda4fd41e406048d111f7e11b7532cb747
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-05-08 17:20:42 +0100

    API fornece listagem de buffets

--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::VenuesController < ActionController::API
     render status: 200, json: venue.as_json(except: [:corporate_name, :registration_number, 
                                                      :user_id, :created_at, :updated_at])
   end
+
+  def index
+    venues = Venue.all 
+    render status: 200, json: venues
+  end
 end
--- spec/requests/apis/venue_api_spec.rb
+++ spec/requests/apis/venue_api_spec.rb
@@ -35,5 +35,49 @@ describe "Venue API" do
       expect(json_response.keys).not_to include("updated_at")
     end
   end
+
+  context "GET /api/v1/venues" do
+    it "success" do
+      # Arrange
+      user = User.create!(email: 'buffet@email.com', password: 'password')
+      venue = Venue.create!(brand_name: "Casa Jardim", corporate_name: "Casa Jardim Buffet Ltda", 
+                            registration_number:"11.111.111/0001-00", address: "Rua Eugênio de Medeiros, 530", 
+                            district: "Pinheiros", city: "São Paulo", state: "SP", zip_code: "05050-050", 
+                            phone_number: "(11)99111-1111", email: "eventosbuffet@email.com", 
+                            description: "Espaços amplos e menus variados.",
+                            payment_methods: "Transferência bancária e pix", user: user)
+
+      other_user = User.create!(email: 'venue@email.com', password: 'password')
+      other_venue = Venue.create!(brand_name: "Buffet Festa", corporate_name: "Buffet Festa Ltda", 
+                                  registration_number: "22.222.222/0002-20", address: "Rua dos Timbiras, 2500",
+                                  district: "Barro Preto", city: 'Belo Horizonte', state: "MG", zip_code: "30140-000", 
+                                  phone_number: "(31)99220-9292", email: "eventos@buffetfesta.com",
+                                  description: "Espaços amplos e menus variados.",
+                                  payment_methods: "Transferência bancária e pix", user: other_user)
+      # Act
+      get '/api/v1/venues'
+
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response.class).to eq Array
+      expect(json_response.length).to eq 2
+      expect(json_response[0]["brand_name"]).to eq 'Casa Jardim'
+      expect(json_response[1]["brand_name"]).to eq 'Buffet Festa'
+    end
+
+    it "return empty if there is no venue" do
+      # Arrange
+      # Act
+      get '/api/v1/venues'
+      
+      # Assert
+      expect(response.status).to eq 200
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response).to eq []
+    end
+  end
   
 end
