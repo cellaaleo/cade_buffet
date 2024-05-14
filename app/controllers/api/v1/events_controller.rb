1712e7b0325dbce914a3ab2f2ef9ff768e4c2ace
Marcella Bortoletto Aleo <cella.aleo@gmail.com>
2024-05-14 15:50:39 +0100

    a API fornece mais detalhes dos eventos

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,9 +2,7 @@ class Api::V1::EventsController < Api::V1::ApiController
   def index
     venue = Venue.find(params[:venue_id])
     events = venue.events
-    render status: 200, json: events.as_json(only: [:id, :venue_id, :name,
-                                                    :minimum_guests_number, 
-                                                    :maximum_guests_number])
+    render status: 200, json: events.as_json(except: [:created_at, :updated_at])
   end
 
   def show
--- app/controllers/api/v1/venues_controller.rb
+++ app/controllers/api/v1/venues_controller.rb
@@ -3,9 +3,7 @@ class Api::V1::VenuesController < Api::V1::ApiController
     venue = Venue.find(params[:id])
     render status: 200, json: venue.as_json(except: [:corporate_name, :registration_number, 
                                                      :user_id, :created_at, :updated_at],
-                                            include: {events: {only: [:id, :venue_id, :name,
-                                                                      :minimum_guests_number, 
-                                                                      :maximum_guests_number]}})
+                                            include: {events: {except: [:created_at, :updated_at]}})
   end
 
   def index
